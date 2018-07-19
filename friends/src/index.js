23435aec3f81be46e15c518c9551e93e897d054c
bdurb <brian@rpmgreenville.com>
2018-07-19 11:53:28 -0700

    setting up index.js with provider, etc

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
